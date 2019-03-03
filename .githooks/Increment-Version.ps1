b03823a5a639908a0dd0b2cf39f4c2074980fc11
Richard D <rmdaustin@gmail.com>
2019-03-03 16:42:30 -0600

    Adding other versioning formats

--- .githooks/Increment-Version.ps1
+++ .githooks/Increment-Version.ps1
@@ -1,4 +1,37 @@
+
+
+
+# increment integer
 $file = "./version.txt"
 $fileVersion = (Get-Content $file | Select -First 1)
 $fileVersion = [int]$fileVersion + 1
 $fileVersion | Set-Content $file
+
+# increment dotted
+$file = "./version-dotted.txt"
+$fileVersion = [version](Get-Content $file | Select -First 1)
+$newVersion = "{0}.{1}.{2}.{3}" -f [int]$fileVersion.Major, [int]$fileVersion.Minor, [int]$fileVersion.Build, ([int]$fileVersion.Revision + 1)
+$newVersion | Set-Content $file
+
+# increment dotted with carry
+$file = "./version-dotted-carried.txt"
+$fileVersion = [version](Get-Content $file | Select -First 1)
+$majorVersion = [int]$fileVersion.Major
+$minorVersion = [int]$fileVersion.Minor
+$buildVersion = [int]$fileVersion.Build
+$revisionVersion = [int]$fileVersion.Revision + 1
+if ($revisionVersion -gt 9) { 
+	$revisionVersion = 0
+	$buildVersion = $buildVersion + 1
+}
+if ($buildVersion -gt 9) { 
+	$buildVersion = 0
+	$minorVersion = $minorVersion + 1
+}
+if ($minorVersion -gt 9) { 
+	$minorVersion = 0
+	$majorVersion = $majorVersion + 1
+}
+
+$newVersion = "{0}.{1}.{2}.{3}" -f $majorVersion, $minorVersion, $buildVersion, $revisionVersion
+$newVersion | Set-Content $file
--- readme.md
+++ readme.md
@@ -1,12 +1,19 @@
-version increment hooks using info from here:
+
+References:
+
+Version increment hooks using info from here:
 
 https://stackoverflow.com/questions/30195025/increment-version-in-a-text-file
+
+Self-amending commit:
+
+https://stackoverflow.com/questions/3284292/can-a-git-hook-automatically-add-files-to-the-commit
 
 How to get the hooks working in Windows
 
 https://www.visualstudiogeeks.com/DevOps/UsingPowerShellForGitHooksWithVstsGitOnWindows
 
-How to put the hooks in the repo
+How to commit the hooks in the repo by redirecting core.hooksPath
 
 https://www.visualstudiogeeks.com/DevOps/UsingGitHooksWithVstsGitOnWindows#githooks-oh-windows
 
@@ -14,6 +21,6 @@ How to figure out the correct core.hooksPath path syntax:
 
 https://github.com/git-for-windows/git/issues/1420
 
-This was the only one that worked:
+This was the only version that worked (Windows):
 
 git config --global core.hooksPath .\.githooks
