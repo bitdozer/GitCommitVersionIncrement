9c65158524ab452935300a071451e39ab732d81e
Richard D <rmdaustin@gmail.com>
2019-05-26 08:30:30 -0500

    Attempt insert actual version number

--- .githooks/Commit-Msg.ps1
+++ .githooks/Commit-Msg.ps1
@@ -1,10 +1,29 @@
 # increment prepend text
-# $commitfile is -commitfile input parameter
-param ($commitfile = $(throw "Commit message file is required."))
-if (Test-Path $commitfile -PathType Leaf)
-{	$fileVersion = (Get-Content $commitfile | Select -First 1) }
+
+#prepend the upcoing version number (plain int version)
+$file = "./version.txt"
+if (Test-Path $file -PathType Leaf)
+{	
+  $fileVersion = (Get-Content $file | Select -First 1) 
+  # $commitfile is -commitfile input parameter
+  param ($commitfile = $(throw "Commit message file is required."))
+  if (Test-Path $commitfile -PathType Leaf)
+  {	
+    $commitmessage = (Get-Content $commitfile) 
+  }
+  else 
+  {	
+    $commitmessage = ""
+  }
+  $commitmessage = "[r" + $fileVersion  +"] " + $commitmessage
+  # write the ammedned message back to the commitfile
+  $commitmessage | Set-Content $commitfile
+}
 else 
-{	$fileVersion = 0 }
-$fileVersion = "[r0000] " + $fileVersion
-$fileVersion | Set-Content $commitfile
+{	
+  # missing version number, do nothing
+  #$fileVersion = 0 
+}
+
+
 
