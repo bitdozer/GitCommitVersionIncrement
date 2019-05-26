4147b223efbade24f19205aa47fe624c3917a412
Richard D <rmdaustin@gmail.com>
2019-05-26 08:36:18 -0500

    Attempt insert actual version number3

--- .githooks/Commit-Msg.ps1
+++ .githooks/Commit-Msg.ps1
@@ -14,20 +14,24 @@ Write-Host $commitfile
 $file = "./version.txt"
 if (Test-Path $file -PathType Leaf)
 {	
+  Write-Host "Found version file"
   $fileVersion = (Get-Content $file | Select -First 1) 
+  Write-Host "Read version"
   if (Test-Path $commitfile -PathType Leaf)
   {	
+    Write-Host "Found commit message file"
     $commitmessage = (Get-Content $commitfile) 
     Write-Host "loaded commit message"
+    $commitmessage = "[r" + $fileVersion  +"] " + $commitmessage
+    # write the ammedned message back to the commitfile
+    $commitmessage | Set-Content $commitfile
   }
   else 
   {	
+    Write-Host "Missing commit message file"
     $commitmessage = ""
     Write-Host "no commit file"
   }
-  $commitmessage = "[r" + $fileVersion  +"] " + $commitmessage
-  # write the ammedned message back to the commitfile
-  $commitmessage | Set-Content $commitfile
 }
 else 
 {	
