13cbfcb306aa7ad0d161fef869b4085bd5888f19
Richard D <rmdaustin@gmail.com>
2019-05-26 09:02:59 -0500

    [r43] Removed diagnostic output

--- .githooks/Commit-Msg.ps1
+++ .githooks/Commit-Msg.ps1
@@ -1,43 +1,26 @@
-# increment prepend text
-# $commitfile is -commitfile input parameter -- must be first line
+# $commitfile is -commitfile input parameter -- must be first non-comment line
 param ($commitfile = $(throw "Commit message file is required."))
 
-Write-Host "Commit-Msg.ps1 invoked as: " -NoNewline
-Write-Host $MyInvocation.Line
-Write-Host $MyInvocation.UnboundArguments
-
-
-Write-Host "arg 1 from ps: " -NoNewline
-Write-Host $commitfile
-
-
-#prepend the upcoing version number (plain int version)
+#prepend the version number (plain int version)
 $file = "./version.txt"
 if (Test-Path $file -PathType Leaf)
 {	
-  Write-Host "Found version file"
   $fileVersion = (Get-Content $file | Select -First 1) 
-  Write-Host "Read version"
   if (Test-Path $commitfile -PathType Leaf)
   {	
-    Write-Host "Found commit message file"
     $commitmessage = (Get-Content $commitfile) 
-    Write-Host "loaded commit message"
     $commitmessage = "[r" + $fileVersion  +"] " + $commitmessage
-    # write the ammedned message back to the commitfile
+    # write the updated message back to the commitfile
     $commitmessage | Set-Content $commitfile
   }
   else 
   {	
-    Write-Host "Missing commit message file"
-    $commitmessage = ""
-    Write-Host "no commit file"
+    # missing commitmessage, do nothing
   }
 }
 else 
 {	
   # missing version number, do nothing
-  #$fileVersion = 0 
 }
 
 
